b2256c31672f1d73cdfe3bc12e55d1cb478f3204
Vinu Antony <sinij7@gmail.com>
2024-03-13 19:55:46 +0530

    9th attempt

--- frontend/src/pages/Doctors/MyAppointments.jsx
+++ frontend/src/pages/Doctors/MyAppointments.jsx
@@ -1,4 +1,4 @@
-// import formatDate from "../../../utils/formateDate";
+
 import { useParams } from 'react-router-dom';
  import { useState,useEffect } from "react";
  import { baseURL } from "../../../../backend/config/db";
@@ -12,7 +12,7 @@ const MyAppointments = () => {
 
 
   const [bookingDetails, setBookingDetails] = useState([]);
-  console.log(bookingDetails,"hdgjysadbfkabsd")
+ 
   const { id } = useParams();
   const doctorInfo=useSelector((state)=>state.docAuth.doctorInfo)
  
@@ -23,7 +23,7 @@ const MyAppointments = () => {
       try {
        
         const response = await  apiInstance.get(`${baseURL}/doctors/booking-details/${docId}`);
-        console.log(response.data,"where is dataaaa")
+        
       
              
             setBookingDetails(response.data);
@@ -37,24 +37,22 @@ const MyAppointments = () => {
     fetchBookingDetails(); 
   }, [id, doctorInfo,docId]);
 
-  // let result=bookingDetails.map((item) => (
-  //   console.log(item)
-  // ))
+  
 
-  // console.log(result,"resulttt");
+  
 
 
 
 
   return (
-    <table className="w-full text-left text-sm text-gray-500">
+    <table className="w-full border-collapse text-left text-sm text-gray-500">
       <thead className="text-xs text-gray-700 uppercase bg-gray-50">
         <tr>
           <th scope="col" className="px-6 py-3">
             Name
           </th>
           <th scope="col" className="px-6 py-3">
-            Gender
+            Blood
           </th>
 
           <th scope="col" className="px-6 py-3">
@@ -64,40 +62,42 @@ const MyAppointments = () => {
             Price
           </th>
           <th scope="col" className="px-6 py-3">
-            Booked on
+            Booked on Date
+          </th>
+          <th scope="col" className="px-6 py-3">
+            Booked on Time
           </th>
         </tr>
       </thead>
       <tbody>
-      {bookingDetails.map((item) => (
-        
-          <tr key={item._id}>
-            <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap">
-              <img src={item.user.photo} className="w-10 h-10 rounded-full" alt="" />
-              <div className="pl-3">
-                <div className="text-base font-semibold">{item.user.name}</div>
-                <div className="text-normal text-gray-500">{item.user.email}</div>
-              </div>
-            </th>
-            <td className="px-6 py-4">{item.user.gender}</td>
-            <td className="px-6 py-4">
-              {item.isPaid ? (
-                <div className="flex items-center">
-                  <div className="h-2.5 w-2.5 rounded-full bg-green-500 mr-2"></div>
-                  Paid
-                </div>
-              ) : (
-                <div className="flex items-center">
-                  <div className="h-2.5 w-2.5 rounded-full bg-red-500 mr-2"></div>
-                  UnPaid
-                </div>
-              )}
-            </td>
-            <td className="px-6 py-4">{item.ticketPrice}</td>
-            {/* <td className="px-6 py-4">{formatDate(item.createdAt)}</td> */}
-          </tr>
-        ))}
-      </tbody>
+  {bookingDetails.map((item) => (
+    <tr key={item._id}>
+      
+     
+      <td className="px-6 py-4">{item.name}</td>
+      <td className="px-6 py-4">{item.blood}</td>
+      <td className="px-6 py-4">
+        {item.isPaid ? (
+          <div className="flex items-center">
+            <div className="h-2.5 w-2.5 rounded-full bg-green-500 mr-2"></div>
+            Paid
+          </div>
+        ) : (
+          <div className="flex items-center">
+            <div className="h-2.5 w-2.5 rounded-full bg-red-500 mr-2"></div>
+            UnPaid
+          </div>
+        )}
+      </td>
+    
+      <td className="px-6 py-4">{item.ticketPrice}</td>
+      <td className="px-6 py-4">{item.date}</td>
+      <td className="px-6 py-4">{item.time}</td>
+      {/* <td className="px-6 py-4">{formatDate(item.createdAt)}</td> */}
+    </tr>
+  ))}
+</tbody>
+
     </table>
   );
 
