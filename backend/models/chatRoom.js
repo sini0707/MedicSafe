d69ae229924fde8e3f39015be9fe62ef42fe9d98
Vinu Antony <sinij7@gmail.com>
2024-05-16 12:30:14 +0530

    bug:chatroom backend model updated

--- backend/models/chatRoom.js
+++ backend/models/chatRoom.js
@@ -1,14 +1,21 @@
 import mongoose from 'mongoose'
 
 const chatRoom = mongoose.Schema({
-    user: {
-         type: mongoose.Schema.Types.ObjectId, ref: 'User' 
-    }, 
-    doctor: {
-         type: mongoose.Schema.Types.ObjectId, ref: 'Doctor'
-    }, 
-    messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ChatMessage' }],
-})
+     user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User' 
+      }, 
+      doctor: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Doctor'
+      }, 
+      messages: [{
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'ChatMessage'
+      }]
+},{
+     timestamps:true
+ });
 
 const ChatRoom = mongoose.model('chatRoom',chatRoom);
 
